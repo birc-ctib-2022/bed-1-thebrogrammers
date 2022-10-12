e1d8a6cf5252b1e9015aa1eb957efd79ff0f7e36
PeterKADam <89773385+PeterKADam@users.noreply.github.com>
2022-10-12 09:10:52 +0200

    testbed

--- src/format_bed_test.py
+++ src/format_bed_test.py
@@ -1,6 +1,15 @@
+import os
+import filecmp
 # This directory will be checked with pytest. It will examine
 # all files that start with test_*.py and run all functions with
 # names that start with test_
+from format_bed import main
 
-def test_1984():
-    assert 2 + 2 == 4
+input 	= "data/input.bed"
+output	= "data/testoutput.bed"
+expected= "data/output.bed"
+
+def test_format_bed():
+
+	os.system("python src/format_bed.py " + input + " " + output)
+	assert filecmp.cmp(output, expected)
